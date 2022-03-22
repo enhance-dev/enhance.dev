ce7a32ed503f575d4bada8e9fa0aeb1420614a56
Ryan Bethel <ryan.bethel@gmail.com>
2022-03-21 22:00:24 -0400

    added auth plugin

---
+++ src/shared/mock-allow.mjs
@@ -0,0 +1,24 @@
+export default {
+  mockProviderAccounts: {
+    'Ryan Bethel': {
+      login: 'ryanbethel',
+      name: 'Ryan Bethel'
+    },
+    'John Doe': {
+      login: 'johndoe',
+      name: 'John Doe'
+    },
+    'Dont Authenticate': {
+      login: 'dontknow',
+      name: 'Dont Authenticate'
+    }
+  },
+  appAccounts: {
+    ryanbethel: {
+      name: 'Ryan Bethel'
+    },
+    johndoe: {
+      name: 'John Doe'
+    }
+  }
+}
