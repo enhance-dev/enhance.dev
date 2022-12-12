80307ac1bd08b5bc3eb287f365b670c2df22fda2
Taylor Beseda <tbeseda@gmail.com>
2022-12-12 14:05:28 -0700

    Create webfinger.mjs

---
+++ app/api/.well-known/webfinger.mjs
@@ -0,0 +1,44 @@
+// ! This doesn't work until Enhance supports dots in paths
+
+/** @type {import('@enhance/types').EnhanceApiFn} */
+export async function get(req) {
+  const axol =
+    req.query.resource === 'acct:axol@enhance.dev'
+      ? {
+        subject: 'acct:axol@enhance.dev',
+        aliases: [
+          'https://fosstodon.org/@enhance_dev',
+          'https://fosstodon.org/users/enhance_dev',
+        ],
+        links: [
+          {
+            rel: 'http://webfinger.net/rel/profile-page',
+            type: 'text/html',
+            href: 'https://fosstodon.org/@enhance_dev',
+          },
+          {
+            rel: 'self',
+            type: 'application/activity+json',
+            href: 'https://fosstodon.org/users/enhance_dev',
+          },
+          {
+            rel: 'http://ostatus.org/schema/1.0/subscribe',
+            template: 'https://fosstodon.org/authorize_interaction?uri={uri}',
+          },
+        ],
+      }
+      : null
+
+  return axol
+    ? {
+      headers: {
+        'Content-Type': 'application/jrd+json; charset=utf-8',
+      },
+      json: axol,
+    }
+    : {
+      headers: { 'Content-Type': 'text/plain' },
+      body: 'Not found',
+      statusCode: 404,
+    }
+}
