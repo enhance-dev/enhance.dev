d84438e36b49338b9e2a5e1569e707336bf27f71
macdonst <simon.macdonald@gmail.com>
2023-02-06 16:12:50 -0500

    Server side analytics
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

---
+++ app/api/cta/views.mjs
@@ -0,0 +1,14 @@
+import data from '@begin/data'
+
+export async function get() {
+  let views = await data.get({
+    table: 'views',
+    key: 'cta',
+  })
+
+  return {
+    json: {
+      views,
+    },
+  }
+}
