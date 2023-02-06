8064f9ca8725ed04f0c71e0bdb901f4b72f86059
macdonst <simon.macdonald@gmail.com>
2023-02-06 15:47:52 -0500

    Add GA to CTA page

--- app/api/cta/$event.mjs
+++ app/api/cta/$event.mjs
@@ -3,10 +3,13 @@ import QRCode from 'qrcode'
 export async function get(req) {
   const { event } = req.pathParameters
   const dataUrl = await QRCode.toDataURL(`https://enhance.dev/cta/${event}`)
+  const gacode =
+    process.env.ARC_ENV === 'production' ? 'G-FQHNPN78V3' : 'G-0ES194BJQ6'
 
   return {
     json: {
       dataUrl,
+      gacode,
     },
   }
 }
