e7f5d803953cda46fe636919afbc518a3edf82b0
macdonst <simon.macdonald@gmail.com>
2023-02-06 16:12:50 -0500

    Server side analytics
    Signed-off-by: macdonst <simon.macdonald@gmail.com>

--- app/api/cta/$event.mjs
+++ app/api/cta/$event.mjs
@@ -1,10 +1,17 @@
 import QRCode from 'qrcode'
+import data from '@begin/data'
 
 export async function get(req) {
   const { event } = req.pathParameters
   const dataUrl = await QRCode.toDataURL(`https://enhance.dev/cta/${event}`)
   const gacode =
     process.env.ARC_ENV === 'production' ? 'G-FQHNPN78V3' : 'G-0ES194BJQ6'
+
+  await data.incr({
+    table: 'views',
+    key: 'cta',
+    prop: event.toLowerCase(),
+  })
 
   return {
     json: {
