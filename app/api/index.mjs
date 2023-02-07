3c537f88f522f1d84af451ce8c3143f4b844dbeb
Taylor Beseda <tbeseda@gmail.com>
2023-02-07 11:04:02 -0700

    newui query check

--- app/api/index.mjs
+++ app/api/index.mjs
@@ -1,6 +1,12 @@
-export async function get() {
-  return {
-    statusCode: 302,
-    location: '/docs/',
-  }
+/** @type {import('@enhance/types').EnhanceApiFn} */
+export async function get({ query }) {
+  const { newui } = query
+
+  if (typeof newui === 'undefined')
+    return {
+      statusCode: 302,
+      location: '/docs/',
+    }
+
+  return {}
 }
