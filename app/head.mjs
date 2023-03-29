a4c1954b0e4f450960d1f4b7d4e46760bb616e08
Cole Peters <cole@colepeters.com>
2023-03-29 11:48:48 -0500

    Update head title, lang, and meta

--- app/head.mjs
+++ app/head.mjs
@@ -5,41 +5,46 @@ import { getStyles } from '@enhance/arc-plugin-styles'
 export default function Head(state) {
   const { store } = state
   const { doc = {} } = store
-  const { title } = doc
+  const { title: docTitle } = doc
+
+  const title = docTitle ? `${docTitle} — Enhance` : 'Enhance'
+
   return /* html */ `
 <!DOCTYPE html>
-<head>
-  <meta charset="UTF-8">
-  <link rel="apple-touch-icon" sizes="180x180" href="${arc.static(
-    '/img/favicon/apple-touch-icon.png'
-  )}">
-  <link rel="icon" type="image/png" sizes="32x32" href="${arc.static(
-    '/img/favicon/favicon-32x32.png'
-  )}">
-  <link rel="icon" type="image/png" sizes="16x16" href="${arc.static(
-    '/img/favicon/favicon-16x16.png'
-  )}">
-  <link rel="manifest" href="${arc.static('/img/favicon/site.webmanifest')}">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  ${getStyles.styleTag()}
-  <link rel="stylesheet" href="${arc.static('/css/docs-colors.css')}" />
-  <link rel="stylesheet" href="${arc.static('/css/docs-highlight.css')}" />
-  <link rel="stylesheet" href="${arc.static('/bundles/docsearch-css.css')}" />
-  <title>${title ? `${title} - Enhance` : `Enhance docs`}</title>
-  <style>
-    @font-face {
-      font-family: "Rubik";
-      src: url("/_public/font/rubik-var.woff2") format("woff2-variations");
-      font-weight: 400 900;
-    }
-    @font-face {
-      font-family: "Rubik";
-      src: url("/_public/font/rubik-italic-var.woff2") format("woff2-variations");
-      font-weight: 400 900;
-      font-style: italic;
-    }
-  </style>
-</head>
+<html lang='en'>
+  <head>
+    <meta charset="UTF-8">
+    <link rel="apple-touch-icon" sizes="180x180" href="${arc.static(
+      '/img/favicon/apple-touch-icon.png'
+    )}">
+    <link rel="icon" type="image/png" sizes="32x32" href="${arc.static(
+      '/img/favicon/favicon-32x32.png'
+    )}">
+    <link rel="icon" type="image/png" sizes="16x16" href="${arc.static(
+      '/img/favicon/favicon-16x16.png'
+    )}">
+    <link rel="manifest" href="${arc.static('/img/favicon/site.webmanifest')}">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    ${getStyles.styleTag()}
+    <link rel="stylesheet" href="${arc.static('/css/docs-colors.css')}" />
+    <link rel="stylesheet" href="${arc.static('/css/docs-highlight.css')}" />
+    <link rel="stylesheet" href="${arc.static('/bundles/docsearch-css.css')}" />
+    <title>${title}</title>
+    <meta name='description' content='The HTML first full stack web framework.' />
+    <style>
+      @font-face {
+        font-family: "Rubik";
+        src: url("/_public/font/rubik-var.woff2") format("woff2-variations");
+        font-weight: 400 900;
+      }
+      @font-face {
+        font-family: "Rubik";
+        src: url("/_public/font/rubik-italic-var.woff2") format("woff2-variations");
+        font-weight: 400 900;
+        font-style: italic;
+      }
+    </style>
+  </head>
 `
 }
