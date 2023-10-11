b770a133cf06f7d36a61388ac7f51b494cc29c99
Cole Peters <cole@colepeters.com>
2023-10-11 10:31:33 -0500

    Fix for Showcase in dark mode

--- app/elements/showcase/project.mjs
+++ app/elements/showcase/project.mjs
@@ -51,7 +51,7 @@ export default function DocShowcase({ html, state }) {
       }
 
       article {
-        background-color: var(--smoke-denim);
+        background-color: var(--smoke);
         border-radius: var(--radius);
         box-shadow: rgba(0, 0, 0, 0.1) 0px 1px 3px 0px,
           rgba(0, 0, 0, 0.06) 0px 1px 2px 0px;
